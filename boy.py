a09f43815a4949a942a02a6bb1f8db5629243b8d
2002OID <ykkim308321@gmail.com>
2023-10-09 15:04:42 +0900

    boy 클래스 statemachine의 상태 Idel클래스(함수 그룹)생성 및 테스트

--- boy.py
+++ boy.py
@@ -1,13 +1,31 @@
 from pico2d import load_image
 
+
+class Idle:
+    @staticmethod  # 클래스를 여러개의 함수를 grouping 하는 용도로 활용 가능
+    def do():
+        print('Idel Doing') #디버깅용
+
+    @staticmethod
+    def enter():
+        print('Idel Entering')  #디버깅용
+
+    @staticmethod
+    def exit():
+        print('Idel Exiting')   #디버깅용
+
+
 class StateMachine:
     def __init__(self):
+        self.cur_state = Idle
         pass
 
     def start(self):
+        self.cur_state.enter()
         pass
 
     def update(self):
+        self.cur_state.do()
         pass
 
     def draw(self):
@@ -23,6 +41,7 @@ class Boy:
         self.image = load_image('animation_sheet.png')
         self.state_machine = StateMachine()
         self.state_machine.start()
+
     def update(self):
         self.frame = (self.frame + 1) % 8
         self.state_machine.update()
