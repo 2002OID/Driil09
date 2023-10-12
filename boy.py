9c05c37d0858649ecd7ea4f70110bd96b531579e
2002OID <ykkim308321@gmail.com>
2023-10-12 12:18:23 +0900

    Auto_Run 구현

--- boy.py
+++ boy.py
@@ -1,7 +1,7 @@
 import math
 
 from pico2d import load_image, get_time
-from sdl2 import SDL_KEYDOWN, SDLK_SPACE, SDLK_RIGHT, SDL_KEYUP, SDLK_LEFT
+from sdl2 import SDL_KEYDOWN, SDLK_SPACE, SDLK_RIGHT, SDL_KEYUP, SDLK_LEFT, SDLK_a
 
 
 def space_down(e):
@@ -11,16 +11,24 @@ def space_down(e):
 def time_out(e):
     return e[0] == 'TIME_OUT'
 
+
 def right_down(e):
     return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_RIGHT
 
+
 def right_up(e):
     return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_RIGHT
+
+
 def left_down(e):
     return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_LEFT
+
+
 def left_up(e):
     return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_LEFT
 
+def a_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_a
 
 class Idle:
     @staticmethod  # 클래스를 여러개의 함수를 grouping 하는 용도로 활용 가능
@@ -28,7 +36,7 @@ class Idle:
         boy.frame = (boy.frame + 1) % 8
         if get_time() - boy.statr_time > 5:
             boy.state_machine.hendle_event(('TIME_OUT', 0))
-        #print('Idel Doing')  # 디버깅용
+        # print('Idel Doing')  # 디버깅용
 
     @staticmethod
     def enter(boy, e):
@@ -37,13 +45,13 @@ class Idle:
         elif boy.action == 1:
             boy.action = 3
         boy.frame = 0
-        boy.statr_time = get_time() #경과시간
-        #print('Idel Entering')  # 디버깅용
+        boy.statr_time = get_time()  # 경과시간
+        # print('Idel Entering')  # 디버깅용
 
     @staticmethod
     def exit(boy, e):
         pass
-        #print('Idel Exiting')  # 디버깅용
+        # print('Idel Exiting')  # 디버깅용
 
     @staticmethod
     def draw(boy):
@@ -55,26 +63,27 @@ class Sleep:
     @staticmethod  # 클래스를 여러개의 함수를 grouping 하는 용도로 활용 가능
     def do(boy):
         boy.frame = (boy.frame + 1) % 8
-        #print('드르렁 드르렁')  # 디버깅용
+        # print('드르렁 드르렁')  # 디버깅용
 
     @staticmethod
     def enter(boy, e):
         boy.frame = 0
-        #print('눕기')  # 디버깅용
+        # print('눕기')  # 디버깅용
 
     @staticmethod
     def exit(boy, e):
         pass
-        #print('일어서기')  # 디버깅용
+        # print('일어서기')  # 디버깅용
 
     @staticmethod
     def draw(boy):
         if boy.action == 2:
-            boy.image.clip_composite_draw(boy.frame * 100, 200, 100, 100, math.pi / 2, '', boy.x - 25,
-                                      boy.y - 25, 100, 100)
+            boy.image.clip_composite_draw(boy.frame * 100, 200, 100, 100, -math.pi / 2, '', boy.x + 25,
+                                          boy.y - 25, 100, 100)
         else:
             boy.image.clip_composite_draw(boy.frame * 100, 300, 100, 100, math.pi / 2, '', boy.x - 25,
                                           boy.y - 25, 100, 100)
+
 
 class Run:
     @staticmethod  # 클래스를 여러개의 함수를 grouping 하는 용도로 활용 가능
@@ -82,14 +91,12 @@ class Run:
         boy.frame = (boy.frame + 1) % 8
         boy.x += boy.dir * 5
 
-
     @staticmethod
     def enter(boy, e):
         if right_down(e) or left_up(e):
             boy.dir, boy.action = 1, 1
         elif left_down(e) or right_up(e):
             boy.dir, boy.action = -1, 0
-
 
     @staticmethod
     def exit(boy, e):
@@ -99,14 +106,44 @@ class Run:
     def draw(boy):
         boy.image.clip_draw(boy.frame * 100, boy.action * 100, 100, 100, boy.x, boy.y)
 
+class AutoRun():
+    @staticmethod  # 클래스를 여러개의 함수를 grouping 하는 용도로 활용 가능
+    def do(boy):
+        boy.frame = (boy.frame + 1) % 8
+        boy.x += boy.dir * 10
+        if boy.dir == 1 and boy.x > 790:
+            boy.dir, boy.action = -1, 0
+        elif boy.dir == -1 and boy.x < 10:
+            boy.dir, boy.action = 1, 1
+        if get_time() - boy.statr_time > 5:
+            boy.state_machine.hendle_event(('TIME_OUT', 0))
+
+    @staticmethod
+    def enter(boy, e):
+        boy.statr_time = get_time()
+        if boy.dir == 1:
+            boy.action = 1
+        else:
+            boy.action = 0
+        pass
+
+    @staticmethod
+    def exit(boy, e):
+        pass
+
+    @staticmethod
+    def draw(boy):
+        boy.image.clip_draw(boy.frame * 100, boy.action * 100, 100, 100, boy.x, boy.y + 25, 150, 150)
+
 class StateMachine:
     def __init__(self, boy):
         self.boy = boy
         self.cur_state = Idle
         self.table = {
-            Idle: {right_down: Run, left_down: Run, left_up: Run, right_up: Run, time_out: Sleep},
-            Run: {right_down: Idle, left_down: Idle, left_up: Idle, right_up: Idle },
+            Idle: {right_down: Run, left_down: Run, left_up: Run, right_up: Run, time_out: Sleep, a_down: AutoRun},
+            Run: {right_down: Idle, left_down: Idle, left_up: Idle, right_up: Idle},
             Sleep: {right_down: Run, left_down: Run, left_up: Run, right_up: Run, space_down: Idle},
+            AutoRun: {right_down: Run, left_down: Run, time_out: Idle}
 
         }
 
